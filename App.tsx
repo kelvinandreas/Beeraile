e587f08e3faf1e2c13206c6290b933bdbfdcc9e4
Dave Christian Thio <85210356+davethio@users.noreply.github.com>
2024-05-18 23:01:16 +0700

    feat: continue progress to training page

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import QuizScreen from './app/screens/QuizScreen';
-import TrainingScreen from './app/screens/TrainingScreen';
+import TrainingScreen from './app/screens/Training/TrainingScreen';
+import Tutorial1 from './app/screens/Training/Tutorial1';
+import Tutorial2 from './app/screens/Training/Tutorial2';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
@@ -18,6 +20,8 @@ function App(): React.JSX.Element {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Training" component={TrainingScreen} />
+        <Stack.Screen name="Tutorial1" component={Tutorial1} />
+        <Stack.Screen name="Tutorial2" component={Tutorial2} />
         <Stack.Screen name="Quiz" component={QuizScreen} />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ app/screens/Training/Tutorial3.tsx
@@ -0,0 +1,109 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {ButtonCustom} from '../../components/ButtonCustom';
+
+const BrailleGrid = () => {
+  const circles = [1, 2, 3, 4, 5, 6];
+  const filledCircles = [1, 2];
+
+  return (
+    <View style={styles.gridContainer}>
+      {circles.map(num => (
+        <View key={num} style={styles.circleContainer}>
+          <View style={styles.circle}></View>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1E1E1E',
+  },
+  text: {
+    color: 'white',
+    fontSize: 14,
+    textTransform: 'uppercase',
+    marginLeft: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'justify',
+  },
+  buttonView: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentView: {
+    flex: 10,
+    justifyContent: 'center',
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 350,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  circleContainer: {
+    width: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  circle: {
+    width: 120,
+    height: 120,
+    borderRadius: 100,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  filledCircle: {
+    backgroundColor: 'white',
+  },
+  emptyCircle: {
+    backgroundColor: '#1E1E1E',
+  },
+  circleText: {
+    color: 'black',
+    fontSize: 46,
+    fontWeight: 'bold',
+  },
+});
+
+function Tutorial1({navigation}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonView}>
+        <ButtonCustom
+          text="Previous"
+          Navigate={() => navigation.navigate('Home')}
+          soundName="previous.mp3"
+        />
+      </View>
+
+      <View style={styles.contentView}>
+        <Text style={styles.text}>{'This is the letter B and number 2.'}</Text>
+        <BrailleGrid />
+      </View>
+
+      <View style={styles.buttonView}>
+        <ButtonCustom
+          text="Next"
+          Navigate={() => navigation.navigate('Tutorial2')}
+          soundName="next.mp3"
+        />
+      </View>
+    </View>
+  );
+}
+export default Tutorial1;
